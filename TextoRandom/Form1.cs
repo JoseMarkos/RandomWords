f59919c10dd7d46797ec2a7490ab8f1be006d4b9
Marcos <jkmarcosb@gmail.com>
2020-02-16 21:02:23 -0600

    Final order answer

--- TextoRandom/Form1.cs
+++ TextoRandom/Form1.cs
@@ -7,6 +7,7 @@ namespace TextoRandom
     public partial class Form1 : Form
     {
         public int finalSentenceWidth = 21;
+        public int locationX = 0;
 
         public Form1()
         {
@@ -24,7 +25,7 @@ namespace TextoRandom
             
             int[] randomSentenceOrder = utils.GetRandomOrder(sentenceSplit.Length);
 
-
+            locationX = labelInstructions.Location.Y;
             int locationY = labelInstructions.Location.Y;
 
             CreateDisorderLabels();
@@ -32,7 +33,7 @@ namespace TextoRandom
 
             void CreateDisorderLabels()
             {
-                int locationX = labelInstructions.Location.X;
+                int positionX = labelInstructions.Location.X;
 
                 foreach (var item in randomSentenceOrder)
                 {
@@ -40,14 +41,14 @@ namespace TextoRandom
                     {
                         Size = new System.Drawing.Size(1, 30),
                         Name = "label" + item,
-                        Location = new System.Drawing.Point(locationX, locationY)
+                        Location = new System.Drawing.Point(positionX, locationY)
                     };
 
-                    this.splitMain.Panel2.Controls.Add(label);
+                    splitMain.Panel2.Controls.Add(label);
 
                     label.AutoSize = true;
                     label.Text = sentenceSplit[item];
-                    locationX += label.Width + 14;
+                    positionX += label.Width + 14;
                     label.BackColor = Color.LightGray;
 
                     label.Click += new EventHandler(ToggleLabelsWidth); //assign click handler
@@ -58,15 +59,13 @@ namespace TextoRandom
 
             void CreateDisplayLabels()
             {
-                int locationX = labelInstructions.Location.X;
-
                 foreach (var item in randomSentenceOrder)
                 {
                     Label label = new Label
                     {
                         Size = new System.Drawing.Size(1, 30),
                         Name = "label" + item + "Display",
-                        Location = new System.Drawing.Point(locationX, locationY)
+                        Location = new System.Drawing.Point(0, locationY)
                     };
 
                     this.splitDisplay.Panel2.Controls.Add(label);
@@ -74,7 +73,6 @@ namespace TextoRandom
                     label.AutoSize = true;
                     label.Visible = false;
                     label.Text = sentenceSplit[item];
-                    locationX += label.Width + 14;
                     label.BackColor = Color.LightGray;
 
                     label.Click += new EventHandler(RemoveLabel); //assign click handler
@@ -89,19 +87,26 @@ namespace TextoRandom
             lbl.BackColor = Color.Gray;
             lbl.ForeColor = Color.Gray;
 
+
             int index = splitMain.Panel2.Controls.IndexOf(lbl);
 
+            splitDisplay.Panel2.Controls[index].Location = new System.Drawing.Point(locationX, 0);
             splitDisplay.Panel2.Controls[index].Visible = true;
+            locationX += lbl.Width + 14;
         }
 
         protected void RemoveLabel(object sender, EventArgs e)
         {
             Label lbl = sender as Label;
+            locationX -= lbl.Width;
+            locationX -= 14;
+
+            lbl.Visible = false;
 
             int index = splitDisplay.Panel2.Controls.IndexOf(lbl);
 
-            labelInstructions.Text = index.ToString();
-
+            splitMain.Panel2.Controls[index].BackColor = Color.LightGray;
+            splitMain.Panel2.Controls[index].ForeColor = Color.Black;
         }
     }
 }
